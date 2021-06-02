1408ab01e259eee25aeb205106ca2a946d7f354d
dean <deanraguso@gmail.com>
2021-06-02 17:15:57 +1000

    Created: Routing, controller, and views for room.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :room_messages
   resources :rooms
   devise_for :users
+  root "rooms#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
